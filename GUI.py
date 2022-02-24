8872f305db6d6eb9fe401f97a0a35e47cbcbfcdf
gabriel luis <gabiluisfreitas@gmail.com>
2022-02-24 18:46:42 +0000

    GUI Tested

--- GUI.py
+++ GUI.py
@@ -1,49 +1,138 @@
-from tkinter import Tk, Button, Label, Text, StringVar, messagebox, Canvas, Radiobutton, BooleanVar, END, DISABLED, NORMAL
+from ast import Num
+import numbers
+from operator import imod
+from tkinter import Tk, Button, Label, Text, Entry, Canvas,  END, DISABLED, NORMAL, messagebox
 from RC4 import *
+from conversor import txt_to_bytes, bytes_to_txt
+import re
 
+# Main window
 base = Tk()
-
 base.title('RC4')
 base.resizable(False, False)
 base.geometry('500x400')
 base.configure(background='#232426')
-
-
+# Canvas
 Canva = Canvas(base, width=500, height=65, bg='#2a2b2d', highlightthickness=0)
 Canva.place(x=0, y=335)
 
-def gui_encrypt():
-    return
+# 
+def gui_RC4(mode):
+    if mode:
+        input = inputText.get("1.0", END).strip()
+        input = txt_to_bytes(input)
 
-def gui_decrypt():
+        keySeed = keySeedText.get().strip()
+        keySeed = txt_to_bytes(keySeed)
+
+        inputBytes.delete("1.0", END)
+        for i in input:
+            inputBytes.insert(END, str(i) + " ")
+        
+        keySeedBytes.delete(0, END)
+        for i in keySeed:
+            keySeedBytes.insert(END, str(i) + " ")
+
+    else:
+        input = inputBytes.get("1.0", END).strip()
+        input = input.split(" ")
+
+        keySeed = keySeedBytes.get().strip()
+        keySeed = keySeed.split(" ")
+
+        for i in range(len(input)):
+            input[i] = int(input[i])
+
+        for i in range(len(keySeed)):
+            keySeed[i] = int(keySeed[i])
+
+        inputText.delete("1.0", END)
+        inputText.insert(END, bytes_to_txt(input))
+
+        keySeedText.delete(0, END)
+        keySeedText.insert(END, bytes_to_txt(keySeed))
+
+    output = RC4(keySeed, input)
+
+    outputText.config(state=NORMAL)
+    outputText.delete("1.0", END)
+    outputText.insert(END, bytes_to_txt(output))
+    outputText.config(state=DISABLED)
+
+    outputBytes.config(state=NORMAL)
+    outputBytes.delete("1.0", END)
+    outputBytes.insert(END, output)
+    outputBytes.config(state=DISABLED)
+
     return
 
 def clear():
+
+    inputText.delete("1.0", END)
+    inputBytes.delete("1.0", END)
+
+    keySeedText.delete(0, END)
+    keySeedBytes.delete(0, END)
+
+    outputText.config(state=NORMAL)
+    outputText.delete("1.0", END)
+    outputText.config(state=DISABLED)
+    outputBytes.config(state=NORMAL)
+    outputBytes.delete("1.0", END)
+    outputBytes.config(state=DISABLED)
+
     return
 
 inputTextLabel = Label(base, text='Input Text')
 inputTextLabel.place(x=30, y=30)
 inputTextLabel.configure(background='#232426', foreground='#F2F2F2')
+inputBytesLabel = Label(base, text='Input Bytes')
+inputBytesLabel.place(x=280, y=30)
+inputBytesLabel.configure(background='#232426', foreground='#F2F2F2')
+
+keySeedTextLabel = Label(base, text='Key Seed')
+keySeedTextLabel.place(x=30, y=130)
+keySeedTextLabel.configure(background='#232426', foreground='#F2F2F2')
+keySeedBytesLabel = Label(base, text='Key Seed Bytes')
+keySeedBytesLabel.place(x=280, y=130)
+keySeedBytesLabel.configure(background='#232426', foreground='#F2F2F2')
 
 outputTextLabel = Label(base, text='Output Text')
-outputTextLabel.place(x=30, y=230)
+outputTextLabel.place(x=30, y=200)
 outputTextLabel.configure(background='#232426', foreground='#BFBFBD')
+outputBytesLabel = Label(base, text='Output Bytes')
+outputBytesLabel.place(x=280, y=200)
+outputBytesLabel.configure(background='#232426', foreground='#BFBFBD')
 
 inputText = Text(base, width=25, height=3)
 inputText.place(x=20, y=60)
 inputText.configure(background='#04BF68', foreground='#232426', borderwidth=0)
+inputBytes = Text(base, width=25, height=3)
+inputBytes.place(x=270, y=60)
+inputBytes.configure(background='#04BF68', foreground='#232426', borderwidth=0)
+
+keySeedText = Entry(base, width=33)
+keySeedText.place(x=20, y=160)
+keySeedText.configure(background='#04BF68', foreground='#232426', borderwidth=0)
+keySeedBytes = Entry(base, width=33)
+keySeedBytes.place(x=270, y=160)
+keySeedBytes.configure(background='#04BF68', foreground='#232426', borderwidth=0)
 
 outputText = Text(base, width=25, height=3)
-outputText.place(x=20, y=260)
-outputText.config(state=DISABLED, bg='#155939', fg='#BFBFBF', borderwidth=0)
+outputText.place(x=20, y=230)
+outputText.config(state=DISABLED, background='#155939', foreground='#BFBFBF', borderwidth=0)
+outputBytes = Text(base, width=25, height=3)
+outputBytes.place(x=270, y=230)
+outputBytes.config(state=DISABLED, background='#155939', foreground='#BFBFBF', borderwidth=0)
 
-Encrypt = Button(base, text='Encrypt', command=lambda: gui_encrypt())
-Encrypt.place(x=30, y=355)
-Encrypt.configure(background='#04BF68', foreground='#155939', borderwidth=0, activebackground='#232426', pady=5, padx=10)
 
-Decript = Button(base, text='Decrypt', command=lambda: gui_decrypt())
-Decript.place(x=110, y=355)
-Decript.configure(background='#04BF68', foreground='#155939', borderwidth=0, activebackground='#232426', pady=5, padx=10)
+cipherText = Button(base, text='Cipher text', command=lambda: gui_RC4(True))
+cipherText.place(x=30, y=355)
+cipherText.configure(background='#04BF68', foreground='#155939', borderwidth=0, activebackground='#232426', pady=5, padx=10)
+
+cipherBytes = Button(base, text='Cipher bytes', command=lambda: gui_RC4(False))
+cipherBytes.place(x=130, y=355)
+cipherBytes.configure(background='#04BF68', foreground='#155939', borderwidth=0, activebackground='#232426', pady=5, padx=10)
 
 Clear = Button(base, text='Clear', command=lambda: clear())
 Clear.place(x=410, y=355)
--- conversor.py
+++ conversor.py
@@ -4,12 +4,20 @@ def txt_to_bin(txt):
 def bin_to_txt(bin):
     return ''.join(chr(int(bin[i:i+8], 2)) for i in range(0, len(bin), 8))
 
+def txt_to_bytes(txt):
+    return [ord(i) for i in txt]
+
+def bytes_to_txt(bytes):
+    return ''.join(chr(i) for i in bytes)
+
 def main():
     while True:
         print("""
         1. Text to binary
         2. Binary to text
-        3. Exit
+        3. Text to bytes
+        4. Bytes to text
+        5. Exit
         """)
         choice = int(input("Enter your choice: "))
         if choice == 1:
@@ -21,6 +29,16 @@ def main():
             print(bin_to_txt(bin))
             input("Press enter to continue...")
         elif choice == 3:
+            txt = input("Enter a text: ")
+            for i in txt_to_bytes(txt):
+                print(i)
+            input("Press enter to continue...")
+        elif choice == 4:
+            bytes = input("Enter a bytes: ")
+            bytes = bytes.split(",")
+            print(bytes_to_txt(bytes))
+            input("Press enter to continue...")
+        elif choice == 5:
             break
         else:
             print("Invalid choice")
